828c41876b47ee7bf683482cc699d995f32dd2f0
pattarapon.me <jame4005@gmail.com>
2020-01-30 20:30:31 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,21 @@
-<?php
-echo "Hello LINE BOT";
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Web Hooks</title>
+	<link rel="stylesheet" href="">
+</head>
+<body>
+	<?php
+		echo "Hello LINE BOT";
+	?>
+	<form action="" method="get" accept-charset="utf-8">
+		UserID
+		<input type="text"><br>
+		Text
+		<input type="text"><br>
+	</form>
+</body>
+</html>
+
